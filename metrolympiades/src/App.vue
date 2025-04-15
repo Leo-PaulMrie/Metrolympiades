4f5b9ae40d960a43be0a1abd7577327fdce71237
BEUVE <beuve225@unicaen.fr>
2025-04-15 20:22:23 +0200

    :sparkles: add content html

--- metrolympiades/src/App.vue
+++ metrolympiades/src/App.vue
@@ -1,24 +1,27 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Login from './components/login.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+   <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
+    
   </header>
 
   <main>
-    <TheWelcome />
+   <!-- <TheWelcome /> -->
+   <Login/>
   </main>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
 }
 
@@ -43,5 +46,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
---
+++ metrolympiades/src/components/login.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1> Connexion</h1>
+    <input type="email" placeholder="Email" >  </input>
+    <input type="password" placeholder="Mot de passe"> </input>
+    <button> Connexion </button>
+    <p> Pas encore de compte <a href=""> Je m'inscris</a></p>
+</template>
+
+<style scoped>
+
+</style>
