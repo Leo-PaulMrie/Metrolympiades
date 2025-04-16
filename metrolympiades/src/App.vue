23715f43bf7dac8cfde9c176feb9a36f5ccca83b
KDual Louane <kdual231@unicaen.fr>
2025-04-16 20:25:48 +0200

    :lipstick: changing sidebar style css

--- metrolympiades/src/App.vue
+++ metrolympiades/src/App.vue
@@ -12,12 +12,26 @@
 
 </script>
 
-<template>
+<template >
 
-  <AppNavbar/>
+  <div class="layout">
+    <AppNavbar/>
+    <div class="page-content"></div>
+  </div>
 
 </template>
 
 <style scoped>
 /* v-if="showNavbar" */
+  .layout {
+    display: flex; /* mettre navbar à gauche */
+    min-height: 100vh;
+  }
+
+  .page-content {
+    flex: 1;
+    padding: 1rem;
+    background-color: white;
+  }
+
 </style>
--- metrolympiades/src/components/AppNavbar.vue
+++ metrolympiades/src/components/AppNavbar.vue
@@ -12,24 +12,29 @@
 
 <template>
 
-  <header class="navbar">
-
-    <div class="navbar_user" >
-      <b> Metrolympiades </b>
-      <!--<h2>{{ user.teamName }}</h2>-->
+<header class="navbar">
+    <div class="navbar_user">
+      <b class="title">Metrolympiades</b>
+      <b class="team"> user.teamName </b>
     </div>
-
     <div class="menus">
-      <TrophyIcon />
-      <a>Classement général</a>
-      <UserGroupIcon />
-      <a>Mon équipe</a>
-      <AdjustmentsHorizontalIcon />
-      <a>Mes matchs</a>
-      <ArrowRightOnRectangleIcon />
-      <a>Se déconnecter</a>
+      <div class="menu-item">
+        <TrophyIcon class="icon" />
+        <a>Classement général</a>
+      </div>
+      <div class="menu-item">
+        <UserGroupIcon class="icon" />
+        <a>Mon équipe</a>
+      </div>
+      <div class="menu-item">
+        <AdjustmentsHorizontalIcon class="icon" />
+        <a>Mes matchs</a>
+      </div>
+      <div class="menu-item">
+        <ArrowRightOnRectangleIcon class="icon" />
+        <a>Se déconnecter</a>
+      </div>
     </div>
-
   </header>
 
 </template>
@@ -38,29 +43,55 @@
 /*
 v-if="user"*/
 
- /* .navbar {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 1rem;
-    border-bottom: 1px solid var(--color-border);
-    position: sticky;
-    top: 0;
-    background-color: var(--color-bg-primary);
-    z-index: 1;
-  }
+.navbar {
+  width: 25em;
+  background-color: #1f2937;
+  color: white;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  height: 100vh;
+  position: sticky;
+  top: 0;
+}
 
-  .navbar__user {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-  }*/
+.navbar_user{
+  margin-bottom: 2em;
+}
 
-  .menus {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-    color: white;
-  }
+.title {
+  font-size: 2.5rem;
+  font-weight: bold;
+}
+
+.team {
+  font-size: 1.5rem;
+}
+
+.menus {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  font-size: 1.5em;
+  margin-left: 1em;
+}
+
+.menu-item {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  cursor: pointer;
+}
+
+.menu-item a {
+  color: white;
+  text-decoration: none;
+}
+
+.icon {
+  width: 2.5em;
+  height: 2.5em;
+}
 
 </style>
